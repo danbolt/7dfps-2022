1187809a654bb0a8403b6d8cc840b3338eede9d6
Daniel Savage <dansvg@gmail.com>
2022-12-13 19:26:33 -0800

    tweaks

--- gameobjects/AttackGoblin.gd
+++ gameobjects/AttackGoblin.gd
@@ -14,9 +14,10 @@ onready var navigation_agent: NavigationAgent = $NavigationAgent
 
 onready var animation_player: AnimationPlayer = $attack_goblin_base/AnimationPlayer
 
-const DOT_FOV_ANGLE = 0.75
+const DOT_FOV_ANGLE = 0.65
+const DOT_FOV_ANGLE_SUSTAIN = 0.501
 
-const RUN_SPEED = 5.3
+const RUN_SPEED = 5.45
 
 func on_animation_done(animation_name):
 	if (animation_name != "die"):
@@ -73,11 +74,13 @@ func _physics_process(delta):
 				chase_repath_time = 0.0
 				navigation_agent.set_target_location(chasing_player.global_translation)
 				animation_player.play("alert")
+				look_at(chasing_player.global_translation, Vector3.UP)
 				break
 	else:
 		var directionToPlayer: Vector3 = ((chasing_player as Spatial).global_translation - global_translation).normalized()
+		var distanceToPlayerSquared: float = ((chasing_player as Spatial).global_translation).distance_squared_to(global_translation)
 		var angleRatio = ourForward.dot(directionToPlayer * -1)
-		if angleRatio < DOT_FOV_ANGLE or not has_los(chasing_player):
+		if  (distanceToPlayerSquared > (6.0 * 6.0) and (angleRatio < DOT_FOV_ANGLE_SUSTAIN - 0.3)) or angleRatio < DOT_FOV_ANGLE_SUSTAIN or not has_los(chasing_player):
 			chasing = false
 			chasing_player = null
 		
@@ -86,17 +89,24 @@ func _physics_process(delta):
 	if (chasing and animation_player.current_animation != "alert"):
 		look_at(chasing_player.global_translation, Vector3.UP)
 		
-		if true:
+		var distanceToPlayerSquared: float = ((chasing_player as Spatial).global_translation).distance_squared_to(global_translation)
+		if distanceToPlayerSquared > 6.0 * 6.0:
 			chase_repath_time += delta
 			if chase_repath_time > 1.1 + randf() * 0.3:
 				navigation_agent.set_target_location(chasing_player.global_translation)
 				
 			var nextSpotToWalkTo = navigation_agent.get_next_location()
+			look_at(nextSpotToWalkTo, Vector3.UP)
 			moveDirection = (nextSpotToWalkTo - global_translation).normalized()
+		else:
+			look_at((chasing_player as Spatial).translation, Vector3.UP)
+			moveDirection = ((chasing_player as Spatial).global_translation - global_translation).normalized()
 		
 	
 	var gravityValue = Vector3.DOWN * 5 if not is_on_floor() else Vector3.ZERO
 	current_velocity = move_and_slide(gravityValue + (moveDirection * RUN_SPEED), Vector3.UP)
+	
+	global_rotation = Vector3(0.0, global_rotation.y, 0.0)
 	
 func _ready():
 	current_velocity = Vector3.ZERO
