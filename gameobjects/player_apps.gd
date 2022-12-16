5e41402aa3f7466954368bcfa88ff010566a4654
Daniel Savage <dansvg@gmail.com>
2022-12-15 16:54:32 -0800

    address raycast bug

--- gameobjects/player_apps.gd
+++ gameobjects/player_apps.gd
@@ -55,7 +55,7 @@ func _ready():
 func _physics_process(delta):
 	
 	var space_state = get_world().direct_space_state
-	var result = space_state.intersect_ray(subcamera.global_translation + subcamera.global_transform.basis.z * 2.5, subcamera.global_transform.basis.z * -100, [], 2)
+	var result = space_state.intersect_ray(subcamera.global_translation + subcamera.global_transform.basis.z * 2.5, subcamera.global_translation + subcamera.global_transform.basis.z * -100, [], 2)
 	
 	if current_hurtbox == null and result and (result.collider is DemonHurtbox):
 		current_hurtbox = result.collider as DemonHurtbox
